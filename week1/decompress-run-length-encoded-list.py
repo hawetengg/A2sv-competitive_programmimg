4337eb005baff2cfc5bc3daac2e17bcd85983c1b
hawetengg <138904058+hawetengg@users.noreply.github.com>
2023-12-08 15:03:53 +0300

    Added solution for Decompress Run-Length Encoded List

---
+++ week1/decompress-run-length-encoded-list.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def decompressRLElist(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        result = []
+        for i in range(0, len(nums), 2):
+            result += [nums[i + 1]] * nums[i]
+        return result
+
+             
+              
+        
