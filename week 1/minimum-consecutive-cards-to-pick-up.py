77ac89adc52d96cdb9ccf0642bcbb23f69ce50d7
hawetengg <138904058+hawetengg@users.noreply.github.com>
2024-01-17 18:50:04 +0300

    Added solution for Minimum Consecutive Cards to Pick Up

--- week 1/minimum-consecutive-cards-to-pick-up.py
+++ week 1/minimum-consecutive-cards-to-pick-up.py
@@ -14,23 +14,4 @@ class Solution:
         else:
             return min_value
 
-    """
-    class Solution:
-    def minimumCardPickup(self, cards: List[int]) -> int:
-        look_up = {}
-        min_value = len(cards) + 1
-
-        for i in range(len(cards)):
-            if cards[i] not in look_up:
-                look_up[cards[i]] = i
-            else:
-                min_value = min(min_value, i - look_up[cards[i]])
-
-        if min_value == len(cards) + 1:
-            return -1
-        else:
-            return min_value
-    """
-                
-
         
