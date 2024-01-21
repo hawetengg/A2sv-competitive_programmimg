7042614104a0b811dee8073fb438b82abe0eb967
hawetengg <138904058+hawetengg@users.noreply.github.com>
2024-01-21 18:44:18 +0300

    Added solution for Remove Element

--- week 1/remove-element.py
+++ week 1/remove-element.py
@@ -1,19 +1,10 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
+        l = 0
 
-        i, j = 0, len(nums)-1
-        if(j == -1): 
-            return 0
-        while(i < j):
-            if(nums[i] == val):
-                while(i < j and nums[j] == val): 
-                    j -= 1
-                nums[i] = nums[j]
-                j -= 1
-                continue
-            i += 1
-        if nums[i] != val:
-            return i + 1
-        else:
-            return i
+        for r in range(len(nums)):
+            if nums[r] != val:
+                nums[l] = nums[r]
+                l += 1
+        return l
         
