fc2e076c4b7eee459e1cf17ccd02edfbac139291
hawetengg <138904058+hawetengg@users.noreply.github.com>
2024-03-08 16:04:34 +0300

    Add solution for Interestingdrink

---
+++ Codeforces/Interestingdrink.py
@@ -0,0 +1,20 @@
+n = int(input())
+nums = list(map(int, input().split()))
+nums.sort()
+days = int(input())
+for i in range(days):
+    target = int(input())
+    count = 0
+    left = 0
+    right = len(nums) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if target >= nums[mid]:
+            count += ((mid - left) + 1)
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    print(count)
